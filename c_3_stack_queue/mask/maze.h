1f254fcd33824781463d9a313fe6b0a4ba597759
myc <164810193@qq.com>
2014-06-06 09:26:42 -0700

    add maze and modify stack

---
+++ c_3_stack_queue/mask/maze.h
@@ -0,0 +1,4 @@
+typedef struct {
+	short int vert;
+	short int horiz;
+} offsets;
---
+++ c_3_stack_queue/mask/stack.h
@@ -0,0 +1,11 @@
+#ifndef STACK
+#define STACK
+
+#define MAX_STACK_SIZE 100
+typedef struct {
+	short int row;
+	short int col;
+	short int dir;
+} element;
+
+#endif
