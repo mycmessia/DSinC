6f739847e951acfbcf0d22e1d6b73477a3af7b7c
myc <164810193@qq.com>
2014-06-18 06:22:30 -0700

    finish new stack

--- c_3_stack_queue/expression/stack.h
+++ c_3_stack_queue/expression/stack.h
@@ -1,42 +1,20 @@
 #ifndef STACK
 #define STACK
 
-#define MAX_STACK_SIZE
+#define MAX_STACK_SIZE 20
 
 typedef struct {
 	int key;
-} item;
+} element;
 
 typedef struct {
-	item space[MAX_STACK_SIZE];
+	element space[MAX_STACK_SIZE];
 	int top;
-} stack;
+} stack, *pstack;
 
-typedef *stack pstack;
-
-void init_stack(pstack pointer)
-{
-	pointer->top = 0;
-	memset(pointer->space, 0, MAX_STACK_SIZE * sizeof(item));
-}
-
-void create_stack(pstack pointer)
-{
-	
-}
-
-int is_full(pstack stack_pointer)
-{
-	if (stack_pointer->top == MAX_STACK_SIZE)
-		return 1;
-	return 0;
-}
-
-int is_empty(pstack stack_pointer)
-{
-	if (stack_pointer->top == -1)
-		return 1;
-	return 0;
-}
+void init_stack(pstack stack_pointer);
+pstack create_stack(void);
+int is_full(pstack stack_pointer);
+int is_empty(pstack stack_pointer);
 
 #endif
